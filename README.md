e089d570a7bfd30d226a135eaec02153cb0309a1
Yash Srivastava <ysrivastava82@gmail.com>
2024-10-11 13:27:25 +0530

    Updated the documentation, made sure secrets aren't leaking and made the streamlit app functional. Made some refactors in the main file as well, just waiting for the analysis.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # SynGen - Synthetic Data Generation
-In the world of AI, data is moat. Unfortunately, finding enough high-quality data can be a huge pain. That's what syngen tries to solve. I've tested synthetic data generation of a variety of models and variety of prompts. There are tweaks you can do to the complexity, variety, and length of real data, giving you a fine control over the data you want.
+In the world of AI, data is moat. Unfortunately, finding enough high-quality data for downstream machine learning tasks(especially for problems where data scarcity is a big issue)can be a huge pain. That's what syngen tries to solve. I've tested synthetic data generation of a variety of models and for a variety of prompts. There are tweaks you can do to the complexity, variety, and length of real data, giving you a fine control over the data you want. Syngen also comes up with both a webapp and a cli tool, so use as you please.
 
 ## Models and Architecture Tested
 3 classes of models were tested, Ollama(Llama3.2), HF(maybe some GPT2 style small model, or phi/Qwen), and commercially avaialble models(Cohere)
@@ -7,12 +7,22 @@ In the world of AI, data is moat. Unfortunately, finding enough high-quality dat
 3 classes of prompts are testsed, these include prompts for diversity, language complexity, and text length. There is also a base prompt to compare against.
 
 ## Problem
-For starters, we will try to generate sythetic reviews. There are 3 prompts based on the three things we mentioned.
+Syngen, as the name suggests, generates synthetic data based on existing data. The synthetic data extends the existing data, and tries to adheres to the properties of it.
+
+For the purpose of this project, we'll work with a "supplements" subset of Amazon reviews [dataset](https://amazon-reviews-2023.github.io/main.html), and try to augment the data.  
 
 ## What factors we considered?
-Let's see. Will update later.
+Using Language Models to augment the dataset gives us fine control over the type to data we can generate - we can just prompt the LM to give exactly what kind of data we need for our use case. For the purpose of this study, we are going to test on three factos, namely - language complexity, divevrsity, and length. Let's try to understand what each of these factor imply
 
-- For HF models, I are sticking our analysis to models which are around 1B parameters, so that our implementation doesn't take too much space. This also gives an idea on sigal/noise ratio for small models.
+- **Language Complexity:** Language complexity, although difficult to define, basically refers how easily the comprehension of the sentence is. Given the real-world dataset, does the augmented data is similar in terms of language complexity?  
+
+- **Language Diversity:** Language Diversity refers to the different ways in which the same sentence can be communicated. For our use case, we study does the augmented data is similar in terms of language diversity?
+
+- **Text Length:** Most users when writing a review, prefer not to write too much, does LMs do that too, or they end up writing really long reviews - which can raise suspicion on the authenticity of the review.
+
+## Assumptions
+
+- For HF models, I are sticking our analysis to models which are around 1B parameters, I don't have the bandwidth to test larger models, so I chose smaller models.
 
 - For commercial LLMS, I chose Cohere models cause their free tier works for my use case, and the command-r-plus model is actually really good.
 
@@ -26,7 +36,40 @@ Let's see. Will update later.
 3. **Similarity to Original Dataset:** How similar the generated synthetic data is to original dataset. Are there any discrepancies?
 
 ## Implementation details
+First, let us see the directory structure.
+```
+- data
+|_ product_asin.csv
+|_ reviews_supplements.csv
 
+- output
+|_ cohere_XXX.csv
+|_ hf_XXX.csv
+|_ ollama_XXX.csv
+
+- app.py
+- main.py
+- README.md
+- requirements.txt
+- .gitignore
+```
+
+To run the application from CLI(after installing and setting up environment variables), run the following command:
+```
+python main.py 
+```
+Additionally, you can pass these options:
+- `--prompt_type`: Type of prompt to use for generating synthetic data. Valid choices are: 'base', 'diversity', 'lang_complexity', 'text_length'
+
+- `--model`: Type of model to use for generating synthetic data. Valid choices are: 'hf', 'cohere', 'ollama'. In HF, "Qwen/Qwen2.5-1.5B-Instruct" is used based on our assumption.
+
+- `--num_samples`: Number of synthetic samples to generate, defaults to 5.
+
+Additionally, to run the web app, run the following command.
+
+```
+streamlit run app.py
+```
 
 ## Results and Analysis
 
--- app.py
+++ app.py
@@ -1,4 +1,10 @@
+import io
+import os
 import streamlit as st
+from dotenv import load_dotenv
+from transformers import pipeline
+
+load_dotenv()
 
 ## Generate an app where we can generate synthetic dataset based on CSV data. Ability to adjust parameters for adjusting dialogue length, topic, diversity, and language m=complexity should also be there.
 st.title("Synthetic Data Generation App")
@@ -8,11 +14,14 @@ st.write("Upload a CSV file to get started.")
 uploaded_file = st.file_uploader("Choose a file")
 
 # Options to steer data generated(how do we use these params?)
-num_options = st.slider("Number of synthetic data entries to generate", 1, 10, 5)
-dialogue_length = st.slider("Dialogue Length", 50, 500, 100)
 topic = st.text_input("Topic", "Health Supplements")
-diversity = st.slider("Diversity", 0, 1, 0.5)
-language_complexity = st.slider("Language Complexity", 0, 1, 0.5)
+
+# Options to tweak the synthetic data generated
+num_options = st.slider("Number of synthetic data entries to generate", 1, 10, 5)
+
+# Select for the type of prompt to use
+prompt_type = st.selectbox("Select Prompt Type", ["Base", "Diversity", "Language Complexity", "Text Length"])
+
 
 
 if uploaded_file is not None:
@@ -24,46 +33,108 @@ if uploaded_file is not None:
             print(df.iloc[i], file=f, sep = ",")
     
     ## Read from any three random enteries from the dataset, and then create a subset of the dataset in the prompt_base
+    if prompt_type == "Text Length":
+        prompt = f"""
+        I have the following CSV data for health supplements:
+        
+        {df.iloc[0]}
+        {df.iloc[1]}
+        {df.iloc[2]}
+    
+        I would like you to generate {num_options} more examples of synthetic data with a dialouge length similar to the example given.Give only the CSV entries(with column names as well), nothing else.
 
-    prompt_base = f"""
-    I have the following CSV data: 
-    {df.iloc[0]}
-    {df.iloc[1]}
-    {df.iloc[2]}
-    Generate a synthetic dataset based on this input. Produce me {num_options} more examples of the data formatted for CSV to augment dataset. Give only the CSV entries.
-    """
+        """
+    elif prompt_type == "Diversity":
+        prompt = f"""
+        I have the following CSV data for health supplements:
+        
+        {df.iloc[0]}
+        {df.iloc[1]}
+        {df.iloc[2]}
+        
+        I would like you to generate {num_options} more examples of synthetic data with a high level of diversity.Give only the CSV entries(with column names as well), nothing else.
+        
+        """
+    elif prompt_type == "Language Complexity":
+        prompt = f"""
+        I have the following CSV data for health supplements:
 
+        {df.iloc[0]}
+        {df.iloc[1]}
+        {df.iloc[2]}
+
+        I would like you to generate {num_options} more examples of synthetic data with a high level of language complexity.Give only the CSV entries(with column names as well), nothing else.
+
+        """
+    elif prompt_type == "Base":
+        prompt = f"""
+        I have the following CSV data for health supplements:
+        
+        {df.iloc[0]}
+        {df.iloc[1]}
+        {df.iloc[2]}
+    
+        I would like you to generate {num_options} more examples of synthetic data based on this input. Give only the CSV entries(with column names as well), nothing else.
+        
+        """
+    else:
+        st.write("Please select a prompt type to proceed.")
+
+    
     ## Now, we can generate synthetic data based on the prompt_base, give user the ability to select the model.
-    model = st.selectbox("Select Model", ["Cohere", "Microsoft Phi-3.5-mini-instruct", "Llama3.2"])
+    model = st.selectbox("Select Model", ["Cohere", "Qwen2.5-1.5B-Instruct", "Llama3.2"])
     if model == "Cohere":
         import cohere
-        co = cohere.ClientV2("5mG9Baoklq9rOXkTSpTvXZ5J6XG4RMGWTHM7p2sp")
+        co = cohere.ClientV2(os.environ["COHERE_API_KEY"])
         response = co.chat(
             model="command-r-plus",
             messages=[
                 {
                     "role": "user",
-                    "content": prompt_base
+                    "content": prompt
                 }
             ]
         )
-        st.write(response.message.content[0].text)
-    elif model == "Microsoft Phi-3.5-mini-instruct":
-        from transformers import pipeline
-        generator = pipeline('text-generation', model='microsoft/Phi-3.5-mini-instruct')
-        response = generator(prompt_base, max_new_tokens=dialogue_length)
-        st.write(response[0]['generated_text'])
+        import io 
+        # Debug: print(response.message.content[0].text)
+        with st.spinner("Generating synthetic data..."):
+            try:
+                df = pd.read_csv(io.StringIO(response.message.content[0].text), sep=",")
+                st.write(df)
+            except Exception as e:
+                st.write(response.message.content[0].text)
+
+    elif model == "Qwen2.5-1.5B-Instruct":
+        generator = pipeline('text-generation', model='Qwen/Qwen2.5-1.5B-Instruct')
+        response = generator(prompt, max_new_tokens=700)
+        
+        # Debug: print(response[0]['generated_text'])
+        with st.spinner("Generating synthetic data..."):
+            try:
+                df = pd.read_csv(io.StringIO(response[0]['generated_text']), sep=",")
+                st.write(df)
+            except Exception as e:
+                st.write(response[0]['generated_text'])
+
+    
     elif model == "Llama3.2":
         import ollama
         response = ollama.chat(model='llama3.2', messages=[
           {
             'role': 'user',
-            'content': prompt_base,
+            'content': prompt,
           },
         ])
-        st.write(response['message']['content'])
+
+        # Debug: print(response['message']['content'])
+        with st.spinner("Generating synthetic data..."):
+            try:
+                df = pd.read_csv(io.StringIO(response['message']['content']), sep=",")
+                st.write(df)
+            except Exception as e:
+                st.write(response['message']['content'])
+
     else:
         st.write("Please select a model to proceed.")
 else:
     st.write("Please upload a file to proceed.")
-
